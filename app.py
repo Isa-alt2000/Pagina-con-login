011661e2c510cb519aeb8cd5b1ba5b891d4f1773
isa <isa.alt2000@gmail.com>
2025-07-17 17:54:02 -0600

    correcciion de errores

--- app.py
+++ app.py
@@ -114,5 +114,10 @@ def user_view():
     
     return render_template('user/view.html', usuarios=usuarios)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
