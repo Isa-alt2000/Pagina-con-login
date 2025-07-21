89fdedb4f69369ec58107ca546e2cfb661594374
Isa-alt2000 <isa.alt2000@gmail.com>
2025-07-21 19:30:18 +0000

    improved interface

--- generate_hash.py
+++ generate_hash.py
@@ -2,3 +2,4 @@ from werkzeug.security import generate_password_hash
 
 print("Hash para admin (admin123):", generate_password_hash('admin123'))
 print("Hash para usuario (usuario123):", generate_password_hash('usuario123'))
+ 
